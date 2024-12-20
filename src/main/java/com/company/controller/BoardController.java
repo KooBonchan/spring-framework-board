b854364951303d5a880f3a45a2286855ec8c4822
garam9koo <garam9koo@gmail.com>
2024-12-20 17:47:56 +0900

    feature: file uploaded successfully saved to c:\upload

--- src/main/java/com/company/controller/BoardController.java
+++ src/main/java/com/company/controller/BoardController.java
@@ -1,5 +1,15 @@
 package com.company.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +23,20 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.company.domain.BoardDTO;
+import com.company.domain.ImageDTO;
 import com.company.domain.PageDTO;
 import com.company.service.BoardService;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @RequestMapping("board")
 @Log4j
 public class BoardController {
+	private String basePath = "C:\\upload";
+	private String thumbnailFolder = "thumbnail";
+	
 	@Autowired
 	private BoardService boardService;
 	
@@ -62,16 +77,26 @@ public class BoardController {
 	public void writeForm () {};
 	@PostMapping({"", "write"})
 	public String write(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
-		String message = "failed to write new document";		
-		for(MultipartFile file : boardDTO.getFiles()) {
-			log.warn(file.getOriginalFilename());
-			log.warn(file.getSize());
+		String message = "failed to write new document";
+		File uploadPath = getPathByDate();
+		List<ImageDTO> images = new ArrayList<>();
+		for(MultipartFile mFile : boardDTO.getFiles()) {
+			try {
+				var image = saveFile(mFile, uploadPath);
+				if(image != null) {
+					images.add(image);
+				}
+			} catch (IOException e) {
+				log.error("Error while saving file: " + e.getMessage());
+			}
 		}
-		if(boardService.register(boardDTO)) {
-			message= "Successfully uploaded your document"; 
-		}
-		redirectAttributes.addFlashAttribute("message", message);
+		log.warn("images: " + images);
 		return "redirect:/board";
+//		if(boardService.register(boardDTO)) {
+//			message= "Successfully uploaded your document"; 
+//		}
+//		redirectAttributes.addFlashAttribute("message", message);
+//		return "redirect:/board";
 	}
 	
 	@GetMapping("update")
@@ -106,6 +131,66 @@ public class BoardController {
 		}
 		redirectAttributes.addFlashAttribute("message", message);
 		return "redirect:/board";
+	}
+	
+	
+	
+	/* FILE UTILS */
+	private File getPathByDate() {
+		String rawPath = new SimpleDateFormat("yy-MM-dd")
+				.format(new Date())
+				.replace("-", File.separator);
+		File path = new File(basePath, rawPath);
+		if( ! path.exists()) {
+			path.mkdirs();
+		}
+		return path;
+	}
+	
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			return contentType.startsWith("image");
+		} catch(Exception ignored) {
+			log.error("non-image file is stored: " + file.toString());
+		}
+		return false;
+	}
+	
+	private ImageDTO saveFile(MultipartFile mFile,
+			File uploadPath) throws IOException {
+		log.info("uploaded file: " + mFile.getOriginalFilename());
+		log.info("uploaded filesize: "+ mFile.getSize());
+		if( ! mFile.getContentType().startsWith("image")) {
+			log.warn("NONE-IMAGE: " + mFile.getOriginalFilename());
+			return null;
+		};
+		String originalFileName = mFile.getOriginalFilename();
+		String ext = originalFileName.substring(originalFileName.lastIndexOf('.'));
+		UUID uuid = UUID.randomUUID();
+		String realFileName = uuid.toString() + ext;
+		
+		ImageDTO imageDTO = new ImageDTO();
+		imageDTO.setFilePath(uploadPath.toString());
+		imageDTO.setOriginalFileName(originalFileName);
+		imageDTO.setRealFileName(realFileName);
+		
+		File realFile = new File(uploadPath, realFileName);
+		mFile.transferTo(realFile);
+		if( ! checkImageType(realFile)) {
+			realFile.delete();
+		}
+		File thumbnailPath = new File(uploadPath, thumbnailFolder);
+		if( ! thumbnailPath.exists()) thumbnailPath.mkdir();
+		try(FileOutputStream thumbnailStream =
+				new FileOutputStream(
+					new File(thumbnailPath, realFileName))){
+			Thumbnailator.createThumbnail(
+				mFile.getInputStream(),
+				thumbnailStream,
+				100, 100);
+		}
+		return imageDTO;
 	}
 	
 }
---
+++ src/main/java/com/company/controller/api/ImageApiController.java
@@ -0,0 +1,10 @@
+package com.company.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/image")
+public class ImageApiController {
+
+}
